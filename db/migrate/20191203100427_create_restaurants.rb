62b434396993f71f55f0515e1fa21da3fc47fab4
Corentin Cuyvers <corentincuyvers123@gmail.com>
2019-12-03 11:08:18 +0100

    added the model restaurant

---
+++ db/migrate/20191203100427_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.integer :recommendation
+      t.string :opening_time
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_163439) do
+ActiveRecord::Schema.define(version: 2019_12_03_100427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.integer "recommendation"
+    t.string "opening_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
