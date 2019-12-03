ba1e823d61c4b645561ec9840b25975a42bbf637
Rigaldo Justine <justine.rigaldo@hotmail.fr>
2019-12-03 15:05:11 +0100

    validations and column added to users

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,3 +1,6 @@
 class Specialty < ApplicationRecord
   has_many :specialty_restaurants, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :country, presence: true
 end
