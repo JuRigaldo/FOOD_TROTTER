d6b0575d6ee8b5be5854c0767c98e62920eec266
Rigaldo Justine <justine.rigaldo@hotmail.fr>
2019-12-04 15:42:42 +0100

    homepage design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'specialties#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :specialties, only: [:index, :show] do
     resources :restaurants, only: [:index, :show]
