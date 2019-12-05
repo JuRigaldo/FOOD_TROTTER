bcebe1e845204ce4325044c5b39c1d702311a944
Annie Puygrenier aboucal9@gmail.com <aboucal9@gmail.com>
2019-12-05 17:22:46 +0100

    design menu for category and time to eat

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,23 @@ import "bootstrap";
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
+// import { menuBurger } from '../plugins/menu_burger';
 
 initMapbox();
+'use strict';
+
+(function() {
+  var body = document.body;
+  var burgerMenu = document.getElementsByClassName('b-menu')[0];
+  var burgerContain = document.getElementsByClassName('b-container')[0];
+  var burgerNav = document.getElementsByClassName('b-nav')[0];
+
+  burgerMenu.addEventListener('click', function toggleClasses() {
+    [body, burgerContain, burgerNav].forEach(function (el) {
+      el.classList.toggle('open');
+    });
+  }, false);
+})();
+
+// export { menuBurger };
+
