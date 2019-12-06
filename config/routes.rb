132c188283873631edbe725fa8263a0357abeb37
Corentin Cuyvers <corentincuyvers123@gmail.com>
2019-12-06 18:16:17 +0100

    links from restaurant#index to restaurant#show is done

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :specialties, only: [:index, :show] do
-    resources :restaurants, only: [:index, :show]
+    get "/restaurants", to: "restaurants#index"
   end
+
+  resources :restaurants, only: [:show]
 
   get '/tagged', to: "restaurants#tagged", as: :tagged
 end
