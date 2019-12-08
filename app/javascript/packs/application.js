ad911bb0635ac93f538bf9b7d7d72992ca5ead70
Annie Puygrenier aboucal9@gmail.com <aboucal9@gmail.com>
2019-12-08 13:19:30 +0100

    design filtres ready

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,12 +12,15 @@ initMapbox();
 (function() {
   var body = document.body;
   var burgerMenu = document.getElementsByClassName('b-menu')[0];
+  var containerHome = document.getElementsByClassName('container')[1];
   var burgerContain = document.getElementsByClassName('b-container')[0];
   var burgerNav = document.getElementsByClassName('b-nav')[0];
 
   burgerMenu.addEventListener('click', function toggleClasses() {
     [body, burgerContain, burgerNav].forEach(function (el) {
       el.classList.toggle('open');
+      containerHome.style.display = containerHome.style.display === 'none' ? '' : 'none';
+
     });
   }, false);
 
